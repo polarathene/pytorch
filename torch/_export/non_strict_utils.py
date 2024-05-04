6bf28ee67099abcdd6569a9f03ad46caab55ea0f
Pian Pawakapan <pp456@cornell.edu>
2024-05-03 17:10:13 -0700

    [export] disable_forced_specializations (#124949)
    Summary:
    By default, some inferred dynamic shapes guards/constraints that are not expressible with the current dynamic shapes language will lead to specialization to the concrete input values provided. If disable_forced_specializations is set to True, we will not specialize, and will not perform runtime checks on such produced guards. Instead, we allow the user to specify arbitrary shapes, and fail during runtime if the inputs are invalid. Constraints expressible with the language (e.g. ranges, linear derived dims) will still be enforced, and behavior for all other guards remains the same.
    
    Cases where we typically specialize are reshapes:
    ```
    x: [4, 6]  # [s0, s1]
    x = x.reshape([x.shape[0] - 1, -1])
    # this emits a guard Mod(s0*s1, s0-1) = 0, we specialize on s0=4, s1=6
    
    x: [4, 6], y: [24]  # [s0, s1], [s2]
    x = x.reshape([-1]) + y
    # this emits a guard s0*s1 = s2, we specialize on s0=4, s1=6, s2=24
    ```
    
    For now only applicable for non-strict mode (need to figure out how to pass this flag into dynamo's call of produce_guards).
    
    
    Test Plan:
    Added test case that checks compilation, runtime, and suggested fixes behavior.
    
    
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng wenzhe-nrv jiayisunx chenyang78 kadeng chauhang
    
    Reviewed By: avikchaudhuri
    
    Differential Revision: D56361177
    
    Pulled By: pianpwk

--- torch/_export/non_strict_utils.py
+++ torch/_export/non_strict_utils.py
@@ -197,6 +197,7 @@ def produce_guards_and_solve_constraints(
     gm: torch.fx.GraphModule,
     equalities_inputs: EqualityConstraint,
     original_signature: inspect.Signature,
+    _disable_forced_specializations: Optional[bool] = False,
 ):
     """
     Given a fake mode, sources pairs corresponding to equal dynamic shape dimensions,
@@ -207,6 +208,7 @@ def produce_guards_and_solve_constraints(
     Additional inputs:
         equalities_inputs: the equality constraints to use for guards
         original_signature: the signature of the forward method
+        _disable_forced_specializations: if True, avoids forced specializations
     """
     shape_env = fake_mode.shape_env
     assert shape_env.tracked_fakes is not None
@@ -222,6 +224,7 @@ def produce_guards_and_solve_constraints(
             input_contexts=input_contexts,
             equalities_inputs=equalities_inputs,
             ignore_static=False,
+            _disable_forced_specializations=_disable_forced_specializations,
         )
     except ConstraintViolationError as e:
         constraint_violation_error = e
@@ -234,7 +237,9 @@ def produce_guards_and_solve_constraints(
         # TODO(avik): Maybe record the constraint violation error instead and replay later?
         assert constraint_violation_error
         raise constraint_violation_error
-    dim_constraints.solve()
+    dim_constraints.solve(
+        _disable_forced_specializations=_disable_forced_specializations
+    )
     dim_constraints.remove_redundant_dynamic_results()
     forced_specializations = dim_constraints.forced_specializations()
     msg = dim_constraints.prettify_results(
